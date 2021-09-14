69340bb2e10180612284871c76effe85f999a73b
Joe <jkuebler35@gmail.com>
2021-09-14 00:02:14 -0400

    working

--- uav.py
+++ uav.py
@@ -23,7 +23,7 @@ class Stop(smach.State):
 
     def execute(self, userdata):
         rospy.loginfo('The UAV has been disarmed')
-	    time.sleep(1)
+	time.sleep(1)
         return 'do_exit'
 
 # main
@@ -37,28 +37,17 @@ def main():
     # Open the container
     with sm:
         # Add states to the container
-        smach.StateMachine.add('MissionConfig', MissionConfig(),
-                               transitions={'config_completed':'OnGroundInactive'})
-        smach.StateMachine.add('OnGroundInactive', OnGroundInactive(),
-                                transitions={'preflight_checks_initiated':'PreflightChecks'})
-        smach.StateMachine.add('PreflightChecks', PreflightChecks(),
-                               transitions={'preflight_checks_failed':'OnGroundInactive','preflight_checks_passed':'Standby'})
-        smach.StateMachine.add('Standby', Standby(),
-                               transitions={'launch_command_received':'Arming'})
-        smach.StateMachine.add('Arming', Arming(),
-                               transitions={'arming_failed':'OnGroundInactive','armed':'TakingOff'})
-        smach.StateMachine.add('TakingOff', TakingOff(),
-                               transitions={'altitude_reached':'PerformCustomMissionTasks','takeoff_aborted':'Land'})
-        smach.StateMachine.add('PerformCustomMission', PerformCustomMissionTasks(),
-                               transitions={'additional_tasks':'PerformCustomMission','recalled':'ReturnHome'})
-        smach.StateMachine.add('ReturnHome', ReturnHome(),
-                               transitions={'reached_home_coordinates':'Land'})
-        smach.StateMachine.add('Land', Land(),
-                               transitions={'landed':'Disarmed'})
-        smach.StateMachine.add('Disarmed', Disarmed(),
-                               transitions={'do_stop':'Stop'})
-        smach.StateMachine.add('Stop', Stop(),
-                               transitions={'do_exit':'do_exit'})
+        smach.StateMachine.add('MissionConfig', MissionConfig(), transitions={'config_completed':'OnGroundInactive'})
+        smach.StateMachine.add('OnGroundInactive', OnGroundInactive(), transitions={'preflight_checks_initiated':'PreflightChecks'})
+        smach.StateMachine.add('PreflightChecks', PreflightChecks(), transitions={'preflight_checks_failed':'OnGroundInactive','preflight_checks_passed':'Standby'})
+        smach.StateMachine.add('Standby', Standby(), transitions={'launch_command_received':'Arming'})
+        smach.StateMachine.add('Arming', Arming(), transitions={'arming_failed':'OnGroundInactive','armed':'TakingOff'})
+        smach.StateMachine.add('TakingOff', TakingOff(), transitions={'altitude_reached':'PerformCustomMissionTasks','takeoff_aborted':'Land'})
+        smach.StateMachine.add('PerformCustomMissionTasks', PerformCustomMissionTasks(), transitions={'additional_tasks':'PerformCustomMissionTasks','recalled':'ReturnHome'})
+        smach.StateMachine.add('ReturnHome', ReturnHome(), transitions={'reached_home_coordinates':'Land'})
+        smach.StateMachine.add('Land', Land(), transitions={'landed':'Disarmed'})
+        smach.StateMachine.add('Disarmed', Disarmed(), transitions={'do_stop':'Stop'})
+        smach.StateMachine.add('Stop', Stop(), transitions={'do_exit':'do_exit'})
 
 
     # Execute SMACH plan
